0b2ff614e59328c65f373ec98e81c39a1de7cb3d
Thomas Vanden Driesch <thomas.vandendriesch@xeryon.com>
2025-03-12 02:10:11 +0000

    Restored to 'b68d9a076633b0b60b5fed80fd458676eed6869f'
    Replit-Restored-To: b68d9a076633b0b60b5fed80fd458676eed6869f

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "@/hooks/use-theme";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="light">
       <App />
